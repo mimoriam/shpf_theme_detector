e9baa452b2fa9093c0cb7d8a11d511e0f31e36d7
Mimoriam <mimoriam420@gmail.com>
2021-12-26 13:38:02 +0500

    Commit #2 - Django settings

---
+++ detect_api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ThemeModel
+
+# Register your models here.
+
+admin.site.register(ThemeModel)
---
+++ detect_api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class ThemeModel(models.Model):
+    # Example: https://www.premiumtrendz.co.uk/
+    theme_url = models.URLField()
+    theme_name = models.CharField(max_length=255, editable=False)
+
+    def __str__(self):
+        return self.theme_url
