2582247b0a1b996bb2b1c297fdcd85c285129a32
Mimoriam <mimoriam420@gmail.com>
2021-12-26 18:43:55 +0500

    deploy

--- shpf_theme_detector/settings.py
+++ shpf_theme_detector/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m^oc#&tfnhl1kv%ga6w%$kt6!cvgoz1k09c@i1_%wyskfknn4i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'sampledomain.com']
 
 # Application definition
 
